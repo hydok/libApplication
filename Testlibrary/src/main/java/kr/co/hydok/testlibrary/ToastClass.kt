040f23e4f64cc6489b6b3bb17320409ff9381e86
hydok <hydok@naver.com>
2020-12-04 10:44:24 +0900

    gg2

--- Testlibrary/src/main/java/kr/co/hydok/testlibrary/ToastClass.kt
+++ Testlibrary/src/main/java/kr/co/hydok/testlibrary/ToastClass.kt
@@ -5,6 +5,6 @@ import android.widget.Toast
 
 object ToastClass {
 
-    fun showToast(context:Context, msg : String): Toast = Toast.makeText(context,msg,Toast.LENGTH_SHORT)
+    fun showToast(context:Context, msg : String) = Toast.makeText(context,msg,Toast.LENGTH_SHORT).show()
     //g호호호호호호호호
 }
